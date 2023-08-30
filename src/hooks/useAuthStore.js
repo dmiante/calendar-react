efe4d1b0739676c0a37475d09bdb769da90a228d
damian <the.dmian@live.cl>
2023-08-30 17:36:16 -0400

    FIX routes url

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -39,11 +39,27 @@ export const useAuthStore = () => {
     }
   }
 
+  const checkAuthToken = async () => {
+    const token = localStorage.getItem('token')
+    if (!token) return dispatch(onLogout())
+
+    try {
+      const { data } = await calendarApi.get('/auth/renew')
+      localStorage.setItem('token', data.token)
+      localStorage.setItem('token-init-date', new Date().getTime())
+      dispatch(onLogin({ name: data.name, uid: data.uid }))
+    } catch (error) {
+      localStorage.clear()
+      dispatch(onLogout())
+    }
+  }
+
   return {
     status,
     user,
     errorMessage,
     startLogin,
-    startRegister
+    startRegister,
+    checkAuthToken
   }
 }
